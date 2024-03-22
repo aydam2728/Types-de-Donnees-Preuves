f6594cb69db3d9770c3feced609a28e567f138c7
aydam2728 <lesdmart@gmail.com>
2024-03-22 17:07:23 +0100

    a modif make passe pas

--- transf.ml
+++ transf.ml
@@ -12,16 +12,14 @@ let rec names_expr = function
   | VarE(x) -> StringSet.singleton x
   | BinOp(_, e1, e2) -> StringSet.union (names_expr e1) (names_expr e2)
   | IfThenElse(e1, e2, e3) -> StringSet.union (names_expr e1) (StringSet.union (names_expr e2) (names_expr e3))
-  | CallE (f, es) -> List.fold_left (fun acc e -> StringSet.union acc (names_expr e)) (StringSet.singleton f) es
-
-  
+  | CallE ( a :: b ) -> StringSet.union (names_expr a) (names_expr b)
 
 (* TODO: implement *)
 let transf_prog (Prog(fdfs, e)) = Prog(fdfs, e)
 
 let rec is_tail_expr fname e = match e with
   | VarE v ->  v != fname
-  | BinOp (_, e1, e2) -> is_tail_expr fname e1 && is_tail_expr fname e2
+  | BinOp (_, e1, e2) -> is_tail_expr fname e1 && is_tail_expr fname e2
   | IfThenElse (e1, e2, e3) -> is_tail_expr fname e1 && is_tail_expr fname e2 && is_tail_expr fname e3
   | CallE (f, es) -> f != fname && List.for_all (is_tail_expr fname) es
   | _ -> true
@@ -55,4 +53,34 @@ let rec transf_prog (Prog (fdfs, e)) =
   match List.map Option.is_some fdfs' with
   | bs when List.for_all (fun b -> b) bs -> Some (Prog (List.map Option.get fdfs', e))
   | _ -> None
-  
+
+
+let rec fac (n : int) : int =
+  if n = 0 then 1 else n * fac (n - 1)
+
+
+let rec fib (n : int) : int =
+  if n = 0 then 0 else if n = 1 then 1 else fib (n - 1) + fib (n - 2)
+
+(* translate to python*)
+
+let rec string_of_py_expr = function
+  | PyConst n -> string_of_int n
+  | PyVar x -> x
+  | PyBinOp (op, e1, e2) -> "(" ^ string_of_py_expr e1 ^ " " ^ op ^ " " ^ string_of_py_expr e2 ^ ")"
+  | PyIfThenElse (e1, e2, e3) -> "if " ^ string_of_py_expr e1 ^ " then " ^ string_of_py_expr e2 ^ " else " ^ string_of_py_expr e3
+  | PyCallE (f, es) -> f ^ "(" ^ String.concat ", " (List.map string_of_py_expr es) ^ ")"
+
+
+let rec string_of_py_fpdefn = function
+  | PyFPDefn (fname, args, e, tail) -> "def " ^ fname ^ "(" ^ String.concat ", " args ^ "): return " ^ string_of_py_expr e
+
+let rec string_of_py_prog = function
+  | Prog (fdfs, e) -> String.concat "\n" (List.map string_of_py_fpdefn fdfs) ^ "\n" ^ string_of_py_expr e
+
+let rec string_of_py_prog_opt = function
+  | Some p -> string_of_py_prog p
+  | None -> "None"
+
+let _ = print_string (string_of_py_prog_opt (transf_prog (Prog ([FPDefn ("fac", ["n"], VarE "n")], CallE ("fac", [Const 5])))))
+
